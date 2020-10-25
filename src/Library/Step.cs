21831dc77d870df153fef9e0d97662d82e5f54a3
Gonzalo Manzzi <gonzalo427@hotmail.com>
2020-10-25 18:36:11 -0300

    Solución del ejercicio

--- src/Library/Step.cs
+++ src/Library/Step.cs
@@ -23,5 +23,18 @@ namespace Full_GRASP_And_SOLID.Library
         public int Time { get; set; }
 
         public Equipment Equipment { get; set; }
+
+        /*
+            Añadimos este método basandonos en Expert ya que la clase Step es la experta en la información
+            necesaria para ejecutar la operación, y también en SRP, para que la clase Recipe no
+            tuviera la responsabilidad de obtener el texto a imprimir del Step, sino que fuera el Step
+            quien se encargue de proporcionar su texto. De esta manera evitamos cargar a Recipe con
+            una responsabilidad más, y a su vez con una razón de cambio más.
+        */
+        public string GetTextToPrint()
+        {
+            return $"{this.Quantity} de '{this.Input.Description}' " +
+                $"usando '{this.Equipment.Description}' durante {this.Time} \n";
+        }
     }
 }
